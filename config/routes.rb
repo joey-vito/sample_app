71aa73b607246086cab5dea690e9cabefad389cc
Joey <joey.johnson7@icloud.com>
2017-01-30 14:32:37 -0500

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
 
   get '/help', to: 'static_pages#help', as: :help
 
@@ -8,6 +9,7 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new', as: :signup
 
-  root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+
+  resources :users
 end
